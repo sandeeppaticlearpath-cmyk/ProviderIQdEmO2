a5607c49a8bd22ab28c94b2ced72951e0411c0cd
sandeeppaticlearpath-cmyk <sandeeppati.clearpath@gmail.com>
2026-03-17 21:31:10 +0530

    Add initial Page component with a message

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>🚀 My ATS is Live!</h1>
+      <p>If you see this, your app is working.</p>
+    </div>
+  );
+}
